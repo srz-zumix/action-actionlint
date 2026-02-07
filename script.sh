d7b5562cd3c9b6d57c7ad68775f99ac4c443a5a8
srz_zumix <zumix.cpp@gmail.com>
2026-02-07 23:11:12 +0900

    fix

--- script.sh
+++ script.sh
@@ -108,7 +108,6 @@ done \
         -reporter="${INPUT_REPORTER}" \
         -filter-mode="${INPUT_FILTER_MODE}" \
         -fail-level="${INPUT_FAIL_LEVEL}" \
-        -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
         -level="${INPUT_LEVEL}" \
         ${INPUT_REVIEWDOG_FLAGS}
 
